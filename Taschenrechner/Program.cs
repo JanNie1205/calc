9ebdfd8cc2765a977485309ae4f2747189d1dbfd
Jan David Niemeyer <jn@nicgs.onmicrosoft.com>
2016-04-21 15:07:29 +0200

    Auslagern in Methoden

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -12,17 +12,13 @@ namespace Taschenrechner
 
         static void Main(string[] args)
         {
-
             // User Story "Addieren": Als Nutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
-            
-
             // Eingabe der zahlen in Console anschließendes speichern als Variablen
-            Console.Write("Erste Zahl eingeben:");
-            string zahl1AlsString = Console.ReadLine();
-            Console.Write("zweite Zahl eingeben:");
-            string zahl2AlsString = Console.ReadLine();
+            string zahl1AlsString = HoleSummanden("Erste zahl eingeben:");
+            string zahl2AlsString = HoleSummanden("Zweite Zahl eingeben:");
 
             //Konvertierung in Gleitkommazahl
+            //TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
             double zahl1 = Convert.ToDouble(zahl1AlsString);
             double zahl2 = Convert.ToDouble(zahl2AlsString);
            
@@ -32,6 +28,14 @@ namespace Taschenrechner
             //Ausgabe des Ergebnisses
             Console.WriteLine("Ergebnis: " + zahl1 + " + " + zahl2 + " = " + ergebnis);
             WarteAufBenutzerEingabe();
+        }
+
+        static string HoleSummanden(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string summand = Console.ReadLine();
+
+            return summand;
         }
 
         static double Addiere(double zahl1, double zahl2)
