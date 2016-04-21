0805ccc6f4cf563fe71034ef3356cfb7916136ec
Jan David Niemeyer <jn@nicgs.onmicrosoft.com>
2016-04-21 14:02:54 +0200

    Addieren in methode ausgelagert

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -14,20 +14,38 @@ namespace Taschenrechner
         {
 
             // User Story "Addieren": Als Nutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
-            double zahl1;
-            double zahl2;
-            double ergebnis;
+            
 
-            // Eingabe der zahlen in Console und Umwandlung in Datentyp int, anschließendes speichern als Variablen
-            Console.WriteLine("Erste Zahl eingeben:");
-            zahl1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("zweite Zahl eingeben:");
-            zahl2 = Convert.ToDouble(Console.ReadLine());
+            // Eingabe der zahlen in Console anschließendes speichern als Variablen
+            Console.Write("Erste Zahl eingeben:");
+            string zahl1AlsString = Console.ReadLine();
+            Console.Write("zweite Zahl eingeben:");
+            string zahl2AlsString = Console.ReadLine();
 
-            //Ausgabe des Ergenisses
-            ergebnis = zahl1 + zahl2;
+            //Konvertierung in Gleitkommazahl
+            double zahl1 = Convert.ToDouble(zahl1AlsString);
+            double zahl2 = Convert.ToDouble(zahl2AlsString);
+           
+            //Ergebnis berechnen
+            double ergebnis = Addiere(zahl1, zahl2);
+
+            //Ausgabe des Ergebnisses
             Console.WriteLine("Ergebnis: " + zahl1 + " + " + zahl2 + " = " + ergebnis);
+            WarteAufBenutzerEingabe();
+        }
+
+        static double Addiere(double zahl1, double zahl2)
+        {
+            double ergebnis = zahl1 + zahl2;
+
+            return ergebnis;
+        }
+
+        static void WarteAufBenutzerEingabe()
+        {
+            Console.WriteLine("Zum Beenden bitte Enter Drücken.");
             Console.ReadLine();
         }
+
     }
 }
