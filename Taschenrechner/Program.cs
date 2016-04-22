d3a267aa66db6c5c242ea6283a531a6439fcc135
Jan David Niemeyer <jn@nicgs.onmicrosoft.com>
2016-04-22 12:36:25 +0200

    IF-Abfrage  der gewünschten Operation und Behandlung von falschen Eingaben

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -9,28 +9,43 @@ namespace Taschenrechner
 
     class Program
     {
-
+        
         static void Main(string[] args)
         {
             // User Story "Addieren": Als Nutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
             // Eingabe der zahlen in Console anschließendes speichern als Variablen
-            string zahl1AlsString = HoleSummanden("Erste zahl eingeben:");
-            string zahl2AlsString = HoleSummanden("Zweite Zahl eingeben:");
+            string zahl1AlsString = HoleEingabe("Erste zahl eingeben:");
+            string operation = HoleEingabe("Bitte gib die zu verwendende Operation an (+ oder -):");
+            string zahl2AlsString = HoleEingabe("Zweite Zahl eingeben:");            
 
             //Konvertierung in Gleitkommazahl
             //TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
             double zahl1 = Convert.ToDouble(zahl1AlsString);
             double zahl2 = Convert.ToDouble(zahl2AlsString);
-           
+            double ergebnis = 0;
+
             //Ergebnis berechnen
-            double ergebnis = Addiere(zahl1, zahl2);
+            if(operation == "+")
+            {
+                 ergebnis = Addiere(zahl1, zahl2);
+            }
+            else if (operation == "-")
+            {
+                 ergebnis = Subtrahiere(zahl1, zahl2);
+            }
+            else
+            {
+                Console.WriteLine("Du hast eine ungültige Auswahl getroffen");
+            }
 
             //Ausgabe des Ergebnisses
-            Console.WriteLine("Ergebnis: " + zahl1 + " + " + zahl2 + " = " + ergebnis);
-            WarteAufBenutzerEingabe();
+            Console.WriteLine("Ergebnis: " + zahl1 + " " + operation + " " + zahl2 + " = " + ergebnis);
+            HoleEingabe("Zum Beenden bitte Enter Drücken.");
         }
 
-        static string HoleSummanden(string ausgabeText)
+
+
+        static string HoleEingabe(string ausgabeText)
         {
             Console.Write(ausgabeText);
             string summand = Console.ReadLine();
@@ -44,12 +59,12 @@ namespace Taschenrechner
 
             return ergebnis;
         }
-
-        static void WarteAufBenutzerEingabe()
+        
+        static double Subtrahiere(double zahl1, double zahl2)
         {
-            Console.WriteLine("Zum Beenden bitte Enter Drücken.");
-            Console.ReadLine();
-        }
+            double ergebnis = zahl1 - zahl2;
 
+            return ergebnis;
+        }
     }
 }
